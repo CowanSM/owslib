1749dbf5db5692d1a0ea23b4024f128f1db70dd7
Kyle Wilcox <kwilcox@asascience.com>
2012-05-15 12:03:31 -0400

    Save output of tests into a scratch directory

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,17 @@
 import os
 
 def open_file(filepath, mode='r'):
-	"""Helper function to open files from within the tests directory."""
-	return open(os.path.join(os.path.dirname(__file__), filepath), mode)
+    """Helper function to open files from within the tests directory"""
+    return open(os.path.join(test_directory(), filepath), mode)
+
+def test_directory():
+    """Helper function to return path to the tests directory"""
+    return os.path.dirname(__file__)
+
+def scratch_directory():
+    """Helper function to return path to the tests scratch directory"""
+    return os.path.join(test_directory(), 'scratch')
+
+def scratch_file(filename):
+    """Helper function to return file path in the tests scratch directory"""
+    return os.path.join(scratch_directory(), filename)
